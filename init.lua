11c6b15224689aa5b0b1f6de84e6831784b90b5d
apothecary <changerofways@gmail.com>
2020-03-03 19:08:48 -0800

    Added end belts. Also added models of the up and down conveyor belts.

--- init.lua
+++ init.lua
@@ -52,7 +52,7 @@ minetest.register_node( "automatica:dev_conveyor_belt", {
 		},
 	},
 	visual_scale = "mesh",
-	mesh = "conveyor_mid.b3d",
+	mesh = "dev_conveyor_mid.b3d",
 	on_timer = function ( pos, elapsed )
 		--print( "On Timer" )
 		local newPos = { x = pos.x, y = pos.y, z = pos.z }
@@ -95,6 +95,267 @@ minetest.register_node( "automatica:dev_conveyor_belt", {
 						}
 					)
 				end
+			end
+		end
+		minetest.get_node_timer( pos ):set( 1, 1 )
+	end,
+	after_place_node = function ( pos, placer, itemstack, pointed_thing )
+		print( "After place" )
+
+		local dir = placer:get_look_dir()
+		local myDir = get_dir( dir )
+		local myMeta = minetest.get_meta( pos )
+		myMeta:set_int( "dir", myDir )
+
+		minetest.get_node_timer( pos ):set( 1, 1 )
+	end,
+	on_place = function( itemstack, placer, pointed_thing )
+		return do_rotation( itemstack, placer, pointed_thing )
+	end,
+	on_punch = function( pos, node, puncher, pointed_thing )
+		print( "on_punch" )
+		print( dump( node ) )
+	end,
+})
+
+minetest.register_node( "automatica:dev_conveyor_slope_up", {
+	description = "conveyor Belt Slope Up (dev)",
+	drawtype = "mesh",
+	paramtype2 = "facedir",
+	sunlight_propogates = true,
+	groups = { cracky = 3; choppy = 1; punch_operable = 1 },
+	collision_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	visual_scale = "mesh",
+	mesh = "dev_conveyor_slope_up.b3d",
+	on_timer = function ( pos, elapsed )
+		--print( "On Timer" )
+		local newPos = { x = pos.x, y = pos.y, z = pos.z }
+		local myNodeAbove = minetest.get_node( newPos )
+		--if myNodeAbove.name == "air" then
+			--print( myNodeAbove.name )
+			--return
+		--end
+
+		local myObjects = minetest.get_objects_inside_radius( newPos, .5 )
+		if myObjects ~= nil and #myObjects >= .5 then
+			for i=1, #myObjects do
+				local myName = myObjects[i]:get_player_name()
+				local myVelocity = myObjects[i]:get_velocity() or
+					myObjects[i]:get_player_velocity()
+				local myMeta = minetest.get_meta( pos )
+				local myDir = myMeta:get_int( "dir" )
+				local myAddingVelocity = {
+					x = 0,
+					y = -3,
+					z = 0
+				}
+				if myDir == 0 then
+					myAddingVelocity.x = .7
+				elseif myDir == 1 then
+					myAddingVelocity.z = -0.7
+				elseif myDir == 2 then
+					myAddingVelocity.x = -0.7
+				elseif myDir == 3 then
+					myAddingVelocity.z = .7
+				end
+				if math.abs(myVelocity.z)+math.abs(myVelocity.x) < 0.5 then
+					local done = myObjects[i]:add_velocity(
+						myAddingVelocity
+					) or myObjects[i]:add_player_velocity(
+						{
+							x = myAddingVelocity.x*4,
+							y = -3,
+							z = myAddingVelocity.z*4
+						}
+					)
+				end
+			end
+		end
+		minetest.get_node_timer( pos ):set( 1, 1 )
+	end,
+	after_place_node = function ( pos, placer, itemstack, pointed_thing )
+		print( "After place" )
+
+		local dir = placer:get_look_dir()
+		local myDir = get_dir( dir )
+		local myMeta = minetest.get_meta( pos )
+		myMeta:set_int( "dir", myDir )
+
+		minetest.get_node_timer( pos ):set( 1, 1 )
+	end,
+	on_place = function( itemstack, placer, pointed_thing )
+		return do_rotation( itemstack, placer, pointed_thing )
+	end,
+	on_punch = function( pos, node, puncher, pointed_thing )
+		print( "on_punch" )
+		print( dump( node ) )
+	end,
+})
+
+minetest.register_node( "automatica:dev_conveyor_slope_down", {
+	description = "Conveyor Belt Slope Down (dev)",
+	drawtype = "mesh",
+	paramtype2 = "facedir",
+	sunlight_propogates = true,
+	groups = { cracky = 3; choppy = 1; punch_operable = 1 },
+	collision_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	visual_scale = "mesh",
+	mesh = "dev_conveyor_slope_down.b3d",
+	on_timer = function ( pos, elapsed )
+		--print( "On Timer" )
+		local newPos = { x = pos.x, y = pos.y, z = pos.z }
+		local myNodeAbove = minetest.get_node( newPos )
+		--if myNodeAbove.name == "air" then
+			--print( myNodeAbove.name )
+			--return
+		--end
+
+		local myObjects = minetest.get_objects_inside_radius( newPos, .5 )
+		if myObjects ~= nil and #myObjects >= .5 then
+			for i=1, #myObjects do
+				local myName = myObjects[i]:get_player_name()
+				local myVelocity = myObjects[i]:get_velocity() or
+					myObjects[i]:get_player_velocity()
+				local myMeta = minetest.get_meta( pos )
+				local myDir = myMeta:get_int( "dir" )
+				local myAddingVelocity = {
+					x = 0,
+					y = -3,
+					z = 0
+				}
+				if myDir == 0 then
+					myAddingVelocity.x = .7
+				elseif myDir == 1 then
+					myAddingVelocity.z = -0.7
+				elseif myDir == 2 then
+					myAddingVelocity.x = -0.7
+				elseif myDir == 3 then
+					myAddingVelocity.z = .7
+				end
+				if math.abs(myVelocity.z)+math.abs(myVelocity.x) < 0.5 then
+					local done = myObjects[i]:add_velocity(
+						myAddingVelocity
+					) or myObjects[i]:add_player_velocity(
+						{
+							x = myAddingVelocity.x*4,
+							y = -3,
+							z = myAddingVelocity.z*4
+						}
+					)
+				end
+			end
+		end
+		minetest.get_node_timer( pos ):set( 1, 1 )
+	end,
+	after_place_node = function ( pos, placer, itemstack, pointed_thing )
+		print( "After place" )
+
+		local dir = placer:get_look_dir()
+		local myDir = get_dir( dir )
+		local myMeta = minetest.get_meta( pos )
+		myMeta:set_int( "dir", myDir )
+
+		minetest.get_node_timer( pos ):set( 1, 1 )
+	end,
+	on_place = function( itemstack, placer, pointed_thing )
+		return do_rotation( itemstack, placer, pointed_thing )
+	end,
+	on_punch = function( pos, node, puncher, pointed_thing )
+		print( "on_punch" )
+		print( dump( node ) )
+	end,
+})
+
+minetest.register_node( "automatica:dev_conveyor_end", {
+	description = "Conveyor Belt End (dev)",
+	drawtype = "mesh",
+	paramtype2 = "facedir",
+	sunlight_propogates = true,
+	groups = { cracky = 3; choppy = 1; punch_operable = 1 },
+	collision_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			 { -0.5, -0.5, -0.5, 0.5, 0, 0.5 },
+		},
+	},
+	visual_scale = "mesh",
+	mesh = "dev_conveyor_end.b3d",
+	on_timer = function ( pos, elapsed )
+		--print( "On Timer" )
+		local newPos = { x = pos.x, y = pos.y, z = pos.z }
+		local myNodeAbove = minetest.get_node( newPos )
+		--if myNodeAbove.name == "air" then
+			--print( myNodeAbove.name )
+			--return
+		--end
+
+		local myObjects = minetest.get_objects_inside_radius( newPos, .5 )
+		if myObjects ~= nil and #myObjects >= .5 then
+			for i=1, #myObjects do
+				local myName = myObjects[i]:get_player_name()
+				local myVelocity = myObjects[i]:get_velocity() or
+					myObjects[i]:get_player_velocity()
+				local myMeta = minetest.get_meta( pos )
+				local myAddingVelocity = {
+					x = myVelocity.x*-1,
+					y = -3,
+					z = myVelocity.z*-1
+				}
+				local done = myObjects[i]:add_velocity(
+					myAddingVelocity
+				) or myObjects[i]:add_player_velocity(
+					{
+						x = myAddingVelocity.x,
+						y = -3,
+						z = myAddingVelocity.z
+					}
+				)
 			end
 		end
 		minetest.get_node_timer( pos ):set( 1, 1 )
